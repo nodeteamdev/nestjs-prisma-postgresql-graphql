87cfa455b54eb5d70c095dd63ad92b15bcfd2061
Oleksandr Boiko <oleksandr.boiko@onix-systems.com>
2024-05-17 14:02:39 +0300

    test: init mock prisma

--- src/auth/auth.resolver.spec.ts
+++ src/auth/auth.resolver.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthResolver } from './auth.resolver';
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+import { PrismaService } from 'src/common/prisma.service';
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
+  let prismaMock: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
+    prismaMock = mockDeep<PrismaClient>();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthResolver],
+      providers: [
+        AuthResolver,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+      ],
     }).compile();
 
     resolver = module.get<AuthResolver>(AuthResolver);
--- src/post/post.resolver.spec.ts
+++ src/post/post.resolver.spec.ts
@@ -1,13 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostResolver } from './post.resolver';
 import { PostService } from './post.service';
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+import { PrismaService } from 'src/common/prisma.service';
 
 describe('PostResolver', () => {
   let resolver: PostResolver;
+  let prismaMock: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
+    prismaMock = mockDeep<PrismaClient>();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostResolver, PostService],
+      providers: [
+        PostResolver,
+        PostService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+      ],
     }).compile();
 
     resolver = module.get<PostResolver>(PostResolver);
